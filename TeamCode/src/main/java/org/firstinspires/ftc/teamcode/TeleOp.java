65953ca884909fae15c458ded7a0c477d9b0d3fb
Vector5233B <vector5233@gmail.com>
2020-10-06 07:48:57 -0400

    Controls Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,13 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 public class TeleOp extends OpMode {
-    DcMotor frontRight, frontLeft, backRight, backLeft;
-    RevRoboticsCoreHexMotor collector;
+    DcMotor frontRight, frontLeft, backRight, backLeft, collector;
+    final double COLLECTPOWER= 0.5;
+
 
     public void init() {
         frontRight = hardwareMap.dcMotor.get("frontRight");
@@ -26,12 +26,45 @@ public class TeleOp extends OpMode {
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        collector = (RevRoboticsCoreHexMotor) hardwareMap.dcMotor.get("collector");
+        collector =hardwareMap.dcMotor.get("collector");
+        collector.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        collector.setDirection(DcMotor.Direction.FORWARD);
+
 
     }
 
     public void loop() {
-
+        setDriveMotors();
+        setCollectorMotor();
     }
 
+    private void setCollectorMotor () {
+        if (gamepad1.right_bumper) {
+            collector.setPower(COLLECTPOWER);
+        } else if (gamepad1.left_bumper) {
+            collector.setPower(-COLLECTPOWER);
+        } else {
+            collector.setPower(0);
+        }
+    }
+
+    private void setDriveMotors(){
+        if(gamepad1.right_stick_y - gamepad1.right_stick_x > 1){
+            frontLeft.setPower(1 - gamepad1.left_stick_x/2);
+            backRight.setPower(1 + gamepad1.left_stick_x/2);
+        }
+        else{
+            frontLeft.setPower((gamepad1.right_stick_y - gamepad1.right_stick_x) - gamepad1.left_stick_x/2);
+            backRight.setPower((gamepad1.right_stick_y - gamepad1.right_stick_x) + gamepad1.left_stick_x/2);
+        }
+        if(gamepad1.right_stick_y + gamepad1.right_stick_x > 1){
+            frontRight.setPower(1 + gamepad1.left_stick_x/2);
+            backLeft.setPower(1 - gamepad1.left_stick_x/2);
+        }
+        else {
+            frontRight.setPower((gamepad1.right_stick_y + gamepad1.right_stick_x) + gamepad1.left_stick_x / 2);
+            backLeft.setPower((gamepad1.right_stick_y + gamepad1.right_stick_x) - gamepad1.left_stick_x / 2);
+        }
+
+    }
 }
