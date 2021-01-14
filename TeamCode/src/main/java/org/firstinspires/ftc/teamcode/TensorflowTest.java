87a8cfdc84db148dab3eec9325bc4fcb70996715
Team 5233 Vector <vector5233@gmail.com>
2021-01-14 08:58:21 -0800

    Update TensorflowTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorflowTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorflowTest.java
@@ -24,24 +24,28 @@ public class TensorflowTest extends LinearOpMode {
     public static final String LABEL_FIRST_ELEMENT = "Quad";
     public static final String LABEL_SECOND_ELEMENT = "Single";
 
-    public void initialize() {
-        //find TFOD_MODEL_ASSET string
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+    public void initializeVuforia(){
+VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
         parameters.vuforiaLicenseKey = "AaEXPAP/////AAABmfSqfgUnFEgorvdIMud+E1ptq5XASvJAx4PQuxM+AlGmUpefMqD8EB2F8xlKK/Z7qD5v0X/DL6t9rcJJDVK78xCXm+ARbXUOKB/xxeX8XAk4WDnQxkM+kR0xV+shs7NXUgJptb5W1JXVhKAnBW+dmtZ6pJCIWtMsDHEPMvN5lU3+QZWECMikOLDm9PnbhMRkur83hvJlpFdiwzp5LDQfxEDin4tuTbSU1FqWjwuMPK/z3q03cCaswJKM5rOT2NYafYHIezh+eK1QuKPDXoGoYKkh7VGhwZenhZi/1BcfuhBP1YFIOQb4DU5QGul+Cz5J58eY+OwE/sjJbQoGX6qsb6zU4BFecspUGYCcMZMRxCbH";
         parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "Id", hardwareMap.appContext.getPackageName());
+}
+
+    public void initializeTfod() {
+ int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "Id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
         tfodParameters.minimumConfidence = 0.6;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
         if (tfod != null) {
                     tfod.activate();
-                }
-    }
+      }
+}
+    
         public void runOpMode(){
 
-        initialize();
+        initializeVuforia();
+        initializeTfod();
         waitForStart();
         // results 2020.12.19 -- "Objects detected 0".   No visual feedback.
             while (opModeIsActive()) {
