c2ab1447f402202bb767eab8a512d384ddc1cd14
Vector5233A <vector5233@gmail.com>
2021-01-18 14:36:45 -0500

    Fixed buttons for intake, sweeper, and launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -7,14 +7,19 @@
     import com.qualcomm.robotcore.hardware.DcMotorSimple;
     import com.qualcomm.robotcore.hardware.Servo;
 
-    import sun.text.resources.th.BreakIteratorInfo_th;
+    //import sun.text.resources.th.BreakIteratorInfo_th;
 
     @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOp", group="Red")
     public class TeleOp extends OpMode {
-    DcMotor frontRight, frontLeft, backRight, backLeft, intake, intakeMotor2, wobbleGoalGrabber;
+    DcMotor frontRight, frontLeft, backRight, backLeft, intake,
+            sweeper, wobbleGoalGrabber, launcher;
     Servo WGS;
     final double COLLECTPOWER = 1.0;
     final double TICKS_PER_REVOLURION = (383.6 * 2);
+    final double LAUNCHER_POWER=1.0;
+    final double LAUNCHER_THRESHHOLD=.5;
+    final double INTAKE_POWER=1.0;
+    final double INTAKE_THRESHOLD=.3;
 
     final double wobbleGoalGrabberPOWER = 0.5;
     boolean if_pressedGp1X = false;
@@ -26,27 +31,31 @@
 
 
     public void init() {
-            /*frontRight = hardwareMap.dcMotor.get("frontRight");
-            frontLeft = hardwareMap.dcMotor.get("frontLeft");
-            backRight = hardwareMap.dcMotor.get("backRight");
-            backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
 
-            frontRight.setDirection(DcMotor.Direction.REVERSE);
-            frontLeft.setDirection(DcMotor.Direction.FORWARD);
-            backRight.setDirection(DcMotor.Direction.REVERSE);
-            backLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
 
-            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);*/
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         intake = hardwareMap.dcMotor.get("intake");
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intake.setDirection(DcMotor.Direction.FORWARD);
-        intakeMotor2= hardwareMap.dcMotor.get("intakeMotor2");
-        intakeMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        intakeMotor2.setDirection(DcMotorSimple.Direction.FORWARD);
+        sweeper= hardwareMap.dcMotor.get("sweeper");
+        sweeper.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        sweeper.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        launcher= hardwareMap.dcMotor.get("launcher");
+        launcher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        launcher.setDirection(DcMotorSimple.Direction.FORWARD);
 
         wobbleGoalGrabber = hardwareMap.dcMotor.get("wobbleGoalGrabber");
         wobbleGoalGrabber.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -61,19 +70,36 @@
     public void loop() {
         setDriveMotors();
         setIntakeMotor();
+        setSweeper();
+        setLauncher();
         setWobbleGoalGrabberEncoder();
     }
 
     private void setIntakeMotor() {
-        if (gamepad1.right_bumper) {
-            intake.setPower(COLLECTPOWER);
-            intakeMotor2.setPower(COLLECTPOWER);
-        } else if (gamepad1.left_bumper) {
-            intake.setPower(-COLLECTPOWER);
-            intakeMotor2.setPower(COLLECTPOWER);
+        if (gamepad2.right_stick_y > INTAKE_THRESHOLD) {
+            intake.setPower(INTAKE_POWER);
+        } else if (gamepad2.right_stick_y < -INTAKE_THRESHOLD) {
+            intake.setPower(-INTAKE_POWER);
         } else {
             intake.setPower(0);
-            intakeMotor2.setPower(0);
+        }
+    }
+
+    private void setSweeper()  {
+        if (gamepad2.right_bumper) {
+            sweeper.setPower(COLLECTPOWER);
+        } else if (gamepad2.left_bumper) {
+            sweeper.setPower(-COLLECTPOWER);
+        } else {
+            sweeper.setPower(0);
+        }
+    }
+
+    private void setLauncher()  {
+        if (gamepad2.right_trigger > LAUNCHER_THRESHHOLD) {
+            launcher.setPower(gamepad2.left_trigger * LAUNCHER_POWER);
+        } else {
+            launcher.setPower(0);
         }
     }
 
@@ -90,7 +116,7 @@
                 wobbleGoalGrabber.setPower(-0.3);
             }else{
                 wobbleGoalGrabber.setPower(0);
-            }
+            } 
         }
         else {
             wobbleGoalGrabber.setPower(0);
