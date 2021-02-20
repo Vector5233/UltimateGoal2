1d0a59b66cae16443e2a6e626e34e03e1650c60f
vector5233 <vector5233@gmail.com>
2021-02-20 22:47:43 +0900

    launcher code in the BaseDriveObject

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueLauncher.java
@@ -58,9 +58,9 @@ public class AutoBlueLauncher extends LinearOpMode {
             drive.wobbleDeliver();
         }
         else if (s == "B"){
-            drive.driveDistance2(.5, 28, 10000);
+            drive.driveDistance2(.5, 38, 10000);
             drive.wobbleDeliver();
-            drive.driveDistance2(.5, -22, 10000);
+            drive.driveDistance2(.5, -36, 10000);
         }
         else {
             drive.driveDistance2(.5, 48, 10000);
